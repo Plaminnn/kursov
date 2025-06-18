3ffd26485feeb2d632c2f990ebd6eb8f4aa4fd1c
Plaminnn <plamseizov09@gmail.com>
2025-06-18 17:44:21 +0300

    Add files via upload

---
+++ jivotniii/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jivotniii
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stoka[] stoki = new Stoka[10];
+            int broiStoki = 0;
+
+            Console.WriteLine("Kolko stoki iskash da vuvedesh?");
+            int broi = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < broi; i++)
+            {
+                Console.WriteLine("Ime na hranata:");
+                string ime = Console.ReadLine();
+               
+
+                Console.WriteLine("Vid jivotno:");
+                string vid = Console.ReadLine();
+
+                stoki[broiStoki] = new Stoka(ime, vid);
+                broiStoki++;
+            }
+
+            Console.WriteLine("Vuvedenite stoki sa:");
+            for (int i = 0; i < broiStoki; i++)
+            {
+                Console.WriteLine($"{i + 1}: {stoki[i].Ime}, {stoki[i].Vid}");
+            }
+
+            // Сортиране по име
+            for (int i = 0; i < broiStoki - 1; i++)
+            {
+                for (int j = i + 1; j < broiStoki; j++)
+                {
+                    if (string.Compare(stoki[i].Ime, stoki[j].Ime) > 0)
+                    {
+                        Stoka temp = stoki[i];
+                        stoki[i] = stoki[j];
+                        stoki[j] = temp;
+                    }
+                }
+            }
+
+            Console.WriteLine("=================");
+            for (int i = 0; i < broiStoki; i++)
+            {
+                Console.WriteLine($"{i + 1}: {stoki[i].Ime}, {stoki[i].Vid}");
+
+            }
+        }
+    }
+}
---
+++ jivotniii/Stoka.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jivotniii
+{
+    internal class Stoka
+    {
+        private string Ime;
+        private string Jivotno;
+        internal readonly object Vid;
+
+        public string ime
+        {
+            get { return Ime; }
+            set { Ime = value; }
+
+        }
+        public string jivotno
+        {
+            get { return Jivotno; }
+            set { Jivotno = value; }
+        }
+        public Stoka(string ime, string jivotno)
+        {
+            Ime = ime;
+            Jivotno = jivotno;
+        }
+
+      
+    }
+}
